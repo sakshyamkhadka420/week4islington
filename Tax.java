83494c3f11f4415b8e91dea4d69a1c8929bb5a77
sakshyamkhadka420 <sakshyamkhadka420@gmail.com>
2025-11-29 15:44:33 +0545

    Tax.java

--- Tax.java
+++ Tax.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
 
-public class Tax {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+public class taxcalculator
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter your annual income (NPR): ");
-        double income = scan.nextDouble();
-        double tax;
+        double income = sc.nextDouble();
+        double tax = 0;
+        if (income <= 5000000) {                              
+            tax = income*0.01;
 
-        if (income > 0 && income <= 500000){
-            tax = income * 0.1;
-        }else if(income >   500000 && income <= 700000){
-            tax = 5000 + (income - 500000) * 0.1;
-        }else if(income > 700000 && income <= 1000000){
-            tax = 15000 + (income - 700000) * 0.2;
-        }else if(income > 1000000 && income <= 2000000){
-            tax = 35000 + (income - 1000000) * 0.3;
-        }else if(income > 2000000 && income <= 5000000){
-            tax = 335000 + (income - 2000000) * 0.36;
-        }else{
-            tax = 1135000 + (income - 5000000) * 0.39;
+        } else if (income <= 7000000) {                      
+            tax = 5000000*0.01+ ((income - 5000000)*0.10);
+
+        } else if (income <= 10000000) {                      
+            tax = 5000000*0.01+(2000000*0.10+((income-7000000)*0.20));
+
+        } else if (income <= 20000000) {                      
+            tax = 5000000*0.01+(2000000*0.10+(3000000*0.20+((income-10000000)*0.30)));
+
+        } else if (income <= 50000000) {                      
+            tax = 5000000*0.01+(2000000*0.10+(3000000*0.20+(10000000*0.30+((income - 20000000)*0.36))));
+
+        } else {                                             
+            tax = 5000000*0.01+(2000000*0.10+(3000000*0.20+(10000000*0.30+(30000000*0.36+((income - 50000000) * 0.39)))));
         }
-        
-        System.out.println("Payable tax income : NPR " + tax);
-        
+
+        System.out.println("Your total tax is: NPR " + tax);
     }
 }
